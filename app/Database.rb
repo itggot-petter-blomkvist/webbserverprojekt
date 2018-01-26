b27601756f535b9563f8d168bb549d3a7c8cfb76
voxed <voxedg@gmail.com>
2018-01-26 20:19:36 +0100

    Added database helper

---
+++ app/Database.rb
@@ -0,0 +1,20 @@
+class Database
+    @@database_file = ""
+
+    def self.set_database_file(file)
+        begin
+            @@database_file == file
+        rescue => error
+            puts error.message
+            puts "Couldn't set database file, database operations will result in error."
+        end
+    end
+
+    def self.open_connection
+        if @@database_file != ""
+            SQLite3::Database.open database_file
+        else
+            puts "No database file defined!"
+        end
+    end
+end
