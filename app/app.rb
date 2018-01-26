4c9e2d0f202dedd2dd97de7dec120a9bb531230a
voxed <voxedg@gmail.com>
2018-01-26 20:19:46 +0100

    Added registration route

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,7 @@
 class App < Sinatra::Base
+    configure do
+    end
+
     before do
         @title="Pollare v1"
     end
@@ -15,4 +18,15 @@ class App < Sinatra::Base
         slim :register
     end
 
+    post '/register' do
+        if @error = User.register(params["username"], params["email"], params["password"], params["password_conf"])
+            @username_error = @error.invalid_username        ? "Username should only contain numbers, letters aswell as the special characters _ and -" : 
+                              @error.invalid_username_length ? "Username should only be 5-16 letters long" : nil
+            @email_error    = @error.invalid_email           ? "Please enter a valid email address" : nil
+            @password_error = @error.no_password_match       ? "Passwords do not match" : nil
+        else
+            slim :login
+        end
+        slim :register
+    end
 end
